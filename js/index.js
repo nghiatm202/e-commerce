a18673d7f2addf68bcfc0433dca465467e4e945a
Tran Minh Nghia <nghiatm202@gmail.com>
2022-03-13 18:33:50 +0700

    Feat(view): Header - Create header and category menu using HTML, SCSS, Slick JS

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// ;(function ($) {
+//   $(function () {
+//     $('#category-menu').slick({
+//       infinite: true,
+//       slidesToShow: 8,
+//       slidesToScroll: 8,
+//       autoplaySpeed: 4000,
+//       arrows: true,
+//     })
+//   })
+// })(jQuery)
+
+$('#category-menu').slick({
+  infinite: true,
+  slidesToShow: 8,
+  slidesToScroll: 8,
+  autoplaySpeed: 4000,
+  arrows: true,
+})
